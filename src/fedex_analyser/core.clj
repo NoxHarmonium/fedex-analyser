65ed450ac3a9fe09e683918c49ad8aca6248366e
Sean Dawson <spdawson@agiledigital.com.au>
2020-03-12 13:12:17 +1100

    Support Github links

--- src/fedex_analyser/core.clj
+++ src/fedex_analyser/core.clj
@@ -9,32 +9,41 @@
     (reduce + (map :count reaction-list)) 
     0))
 
-(defn conversation-stream [connection channel-id cursor]
-    (lazy-seq
-     (println "Fetching history for channel " channel-id " cursor " cursor)
-     (let [history (clj-slack.conversations/history connection channel-id (if cursor {:cursor cursor} {}))]
-       (concat (:messages history)
-               (when-let [next-cursor (get-in history [:response_metadata :next_cursor])]
-                 (conversation-stream connection channel-id next-cursor))))))
+(defn conversation-stream
+  "Returns a lazy stream of conversation messages, when a page is consumed, the next page will be fetched"
+  [connection channel-id cursor]
+  (lazy-seq
+   (println "Fetching history for channel " channel-id " cursor " cursor)
+   (let [response (clj-slack.conversations/history connection channel-id (if cursor {:cursor cursor} {}))]
+     (assert (:ok response) (:error response))
+     (concat (:messages response)
+             (when-let [next-cursor (get-in response [:response_metadata :next_cursor])]
+               (conversation-stream connection channel-id next-cursor))))))
 
-(defn extract-attachment-data [message]
+(defn extract-attachment-data 
+  "Extracts the useful data out of the unstructured attachment data"
+  [message]
   (when-let [attachments (:attachments message)] 
-    (let [link-attachments (filter :original_url attachments)]
-      (select-keys (last link-attachments) [:author_name :fallback :original_url]))))
+    (let [projected (select-keys (last attachments) [:author_name :title :fallback :original_url :app_unfurl_url])]
+      (assoc projected 
+             :title (or (:title projected) (:fallback projected))
+             :url (or (:original_url projected) (:app_unfurl_url projected))))))
   
 
 (defn -main
   "Crawl the Fedex channel and get the links with the highest reaction count"
   [& _]
   (let [connection {:api-url "https://slack.com/api" :token (env :slack-api-token)}
-        channels (:channels  (clj-slack.conversations/list connection))
+        response (clj-slack.conversations/list connection)
+         _ (assert (:ok response) (:error response))
+        channels (:channels response)
         fedex-convo (first (filter #(= (:name %) "fedex") channels))
         channel-id (:id fedex-convo)
-        messages (conversation-stream connection channel-id nil)
+        messages (conversation-stream connection channel-id nil) 
         with-links (map #(merge % (extract-attachment-data %)) messages)
-        cleaned (filter #(and (:original_url %)) with-links)
-        projected (map #(select-keys % [:author_name :fallback :original_url :reactions]) cleaned)
-        aggregrated (map #(assoc % :reactions (sum-reactions (:reactions %))) projected)
+        projected (map #(select-keys % [:author_name :title :url :reactions]) with-links)
+        cleaned (filter :url projected)
+        aggregrated (map #(update-in % [:reactions] sum-reactions) cleaned)
         sorted (reverse (sort-by :reactions (reverse aggregrated))) ; Reverse to put newer links at the top
         json (cc/generate-string sorted {:pretty true})]
     (spit "results.json" json)))
